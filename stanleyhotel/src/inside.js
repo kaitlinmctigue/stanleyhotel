71fbf6feb046128a7e18ae5847c0eef49af48ba3
Kaitlin McTigue <kaitlinmctigue@Kaitlins-MBP.verizon.net>
2018-10-25 23:37:49 -0400

    routing

---
+++ stanleyhotel/src/inside.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inside extends Component {
+    render() {
+        return (
+            <div className="App">
+                inside
+            </div>
+        );
+    }
+}
+
+export default Inside;
---
+++ stanleyhotel/src/outside.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Outside extends Component {
+    render() {
+        return (
+            <div className="App">
+                outside
+            </div>
+        );
+    }
+}
+
+export default Outside;
