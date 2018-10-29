9589811a1b4c19b3cf4a1bf6dced6e9915f1e5b2
Katie McTigue <kaitlinmctigue@gmail.com>
2018-10-28 20:56:19 -0400

    Updates

---
+++ precache-manifest.db4ef118243f681c024693b6c2461fd4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8915189914628001c47e43bb839389f7",
+    "url": "/stanleyhotel/static/media/lobby.89151899.jpg"
+  },
+  {
+    "revision": "b2b648ca4fc91a0ff98d",
+    "url": "/stanleyhotel/static/css/main.cad0f57a.chunk.css"
+  },
+  {
+    "revision": "4fe91c660f1f682264e5fe4be4828a83",
+    "url": "/stanleyhotel/static/media/paper.4fe91c66.svg"
+  },
+  {
+    "revision": "322304a07c4edade18db",
+    "url": "/stanleyhotel/static/js/1.322304a0.chunk.js"
+  },
+  {
+    "revision": "b772ac66791c479895e5",
+    "url": "/stanleyhotel/static/js/runtime~main.b772ac66.js"
+  },
+  {
+    "revision": "f038a4bf889cf35888ba85ff579381f3",
+    "url": "/stanleyhotel/static/media/bg.f038a4bf.jpg"
+  },
+  {
+    "revision": "d246e03b035817487c5950a321b1a059",
+    "url": "/stanleyhotel/static/media/bar.d246e03b.jpg"
+  },
+  {
+    "revision": "b2b648ca4fc91a0ff98d",
+    "url": "/stanleyhotel/static/js/main.b2b648ca.chunk.js"
+  },
+  {
+    "revision": "e9fe5a775d4630b290a29922e4c1bacd",
+    "url": "/stanleyhotel/static/media/lounge.e9fe5a77.jpg"
+  },
+  {
+    "revision": "c847b324e204ebe87c47cfb4850ae44a",
+    "url": "/stanleyhotel/static/media/maze.c847b324.jpg"
+  },
+  {
+    "revision": "d9606e1410f21d7b4b409d36e5863ab5",
+    "url": "/stanleyhotel/static/media/room.d9606e14.jpg"
+  },
+  {
+    "revision": "2c34557621f45fdbbe77e489ea8454e2",
+    "url": "/stanleyhotel/static/media/open.2c345576.png"
+  },
+  {
+    "revision": "7575b840b9832f1ed2b6b97f8c55973a",
+    "url": "/stanleyhotel/static/media/closed.7575b840.png"
+  },
+  {
+    "revision": "322304a07c4edade18db",
+    "url": "/stanleyhotel/static/css/1.81a09fec.chunk.css"
+  },
+  {
+    "revision": "396e820d6238e2b6200ca44a9834e309",
+    "url": "/stanleyhotel/index.html"
+  }
+];
